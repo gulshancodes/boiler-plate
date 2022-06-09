0696739dbb443f110b8ad657a49ca6be2bb7f3b1
Gulshan Songara <64693731+gulshancodes@users.noreply.github.com>
2022-06-09 09:42:45 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+'use strict';
+
+/* Helper Functions */
+
+window.qs = (selector, parent = document) => parent.querySelector(selector);
+
+window.qsa = (selector, parent = document) => parent.querySelectorAll(selector);
+
